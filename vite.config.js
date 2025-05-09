40c12b1fa2f5e939373722e60268265fa26c65e5
imad mohamed <abmohammedimad00@gmail.com>
2025-05-09 12:54:34 +0400

    Update vite.config.js
    DEPLOY

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  base: process.env.VITE_BASE_PATH
+  base: process.env.VITE_BASE_PATH || "imad-Portfolio",
 })
